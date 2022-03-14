496a58e339cb94af71b4094736d3bdd3c5ce596c
= <=>
2022-03-14 22:08:05 +0500

    Composer updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+        
         View::composer(['layouts.app', 'dashboard.layouts.app'], function ($view) {
             $view->with('route', Route::currentRouteName());
         });
